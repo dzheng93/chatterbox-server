9a8262137d5bb2d18e3b40cfa107eed8c6e5ca9d
dzheng93 <dunstanzheng@gmail.com>
2016-11-18 20:05:01 -0800

    Jonos

--- server/request-handler.js
+++ server/request-handler.js
@@ -28,7 +28,8 @@ var defaultCorsHeaders = {
   'access-control-allow-headers': 'content-type, accept',
   'access-control-max-age': 10 // Seconds.
 };
-var messages = {results: []};
+
+var messages = {results: [{username: 'Fred', roomname: 'lobby', text: 'Welcome'}]};
 
 module.exports.requestHandler = function(request, response) {
   // Request and Response come from node's http module.
@@ -49,50 +50,27 @@ module.exports.requestHandler = function(request, response) {
 
   // The outgoing status.
   var statusCode = 200;
-  var getToClient = [];
 
   // check the endpoint
   var headers = defaultCorsHeaders;
-  headers['Content-Type'] = 'text/html';
+  headers['Content-Type'] = 'application/json';
 
   if (request.url === '/classes/messages') {
     //
-    if (request.method === 'GET') {
-      if (messages.length < 100) {
-        getToClient = messages.results;
-      } else {
-        getToClient = messages.results.slice(0, 100);
-      }
+    if (request.method === 'GET' || request.method === 'OPTIONS') {
       response.writeHead(statusCode, headers);
-      response.end(JSON.stringify({'results': getToClient}));
+      response.end(JSON.stringify(messages));
     } else if (request.method === 'POST') {
       request.on('data', function (data) {
-        messages.results.push(JSON.parse(data));
+        messages.results.unshift(JSON.parse(data));
       });
       statusCode = 201;
       response.writeHead(statusCode, headers);
-      response.end();
-    }
+      response.end('{"success" : "Updated Successfully", "status" : 200}');
+    } 
   } else {
     statusCode = 404;
     response.writeHead(statusCode, headers);
     response.end();
-  }
-  // See the note below about CORS headers.
-  // Tell the client we are sending them plain text.
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  // response.end(JSON.stringify('hi'));
-  
+  }  
 };
